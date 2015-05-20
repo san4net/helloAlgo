ab31ca457828490b0f779281eb0df576803aa9cb
san4net <san4net@gmail.com>
2015-05-20 09:42:04 +0530

    heap update

--- src/main/java/com/ds/template/impls/Heap.java
+++ src/main/java/com/ds/template/impls/Heap.java
@@ -1,53 +1,55 @@
 package com.ds.template.impls;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class Heap {
 	Integer[] elements;
-	int capacity;
-	int size;
+	int size;  // no of elements in the heap
+	int capacity ; // capacity of heap
 
-	public Heap(Integer[] elements, int capacity) {
+	public Heap(int[] elements, int capacity) {
 		super();
-		this.elements = elements;
-		this.capacity = capacity;
+		this.elements = new Integer[capacity];
+		Arrays.copyOf(elements,capacity );
 		this.size = elements.length;
 	}
 
-	public int parent(int i) {
-		if (i >= capacity)
+	public int getParentIndex(int i) {
+		if (i<0  || i >= size)
 			return -1;
 		return (i - 1) / 2;
 	}
 
-	public static int leftChild(int i) {
+	public static int getLeftChildIndex(int i) {
+		
 		int childIndex = i * 2 + 1;
 		// if(childIndex>=capacity) return -1;
 		return childIndex;
 	}
 
-	public static int rightChild(int i) {
+	public static int getRightChildIndex(int i) {
 		int childIndex = i * 2 + 2;
 		// if(childIndex>=capacity) return -1;
 
 		return childIndex;
 	}
 
-	// this is percolate down
+	// this is percolate down this is utilty
+	
 	public static void maxHeapify(Integer[] array, int index) {
-		int leftIndex = leftChild(index);
-		int rightIndex = rightChild(index);
+		int leftIndex = getLeftChildIndex(index);
+		int rightIndex = getRightChildIndex(index);
 
 		int maxIndex = -1;
-
-		if (leftIndex < array.length && array[leftIndex] > array[index]) {
+		
+		// if max heapify
+		if (leftIndex < array.length  && array[leftIndex] > array[index]) {
 			maxIndex = leftIndex;
 		} else {
 			maxIndex = index;
 		}
 
-		if (rightIndex < array.length && array[rightIndex] > array[maxIndex]) {
+		if (rightIndex < array.length  && array[rightIndex] > array[maxIndex]) {
 			maxIndex = rightIndex;
 		}
 
@@ -57,68 +59,132 @@ public class Heap {
 			maxHeapify(array, maxIndex);
 		}
 	}
-// minHeapify
+   
+	// this is percolate down this is utilty
+	
+	public void maxHeapify(int index) {
+		int leftIndex = getLeftChildIndex(index);
+		int rightIndex = getRightChildIndex(index);
 
-	private static void exchange(Integer[] array, int index, int largest) {
-		int temp = array[index];
-		array[index] = array[largest];
-		array[largest] = temp;
+		int maxIndex = -1;
+		
+		// if max heapify
+		if (leftIndex < size  && elements[leftIndex] > elements[index]) {
+			maxIndex = leftIndex;
+		} else {
+			maxIndex = index;
+		}
+
+		if (rightIndex < size  && elements[rightIndex] > elements[maxIndex]) {
+			maxIndex = rightIndex;
+		}
+
+		if (index != maxIndex) {
+			// swap the elements
+			exchange(elements, index, maxIndex);
+			maxHeapify(maxIndex);
+		}
+	}
+	
+	public static void minHeapify(Integer[] array, int index){
+		int leftChildIndex = getLeftChildIndex(index);
+		int rightChildIndex = getRightChildIndex(index);
+		int minIndex = -1;
+		
+		if(leftChildIndex >=0 && array[leftChildIndex] <array[index]){
+			minIndex = leftChildIndex;
+		}else {
+			minIndex = index;
+		}
+		
+		if(rightChildIndex >=0 && array[rightChildIndex] <array[minIndex]){
+			minIndex = rightChildIndex;
+		}
+		
+		if(index != minIndex){
+			// swap and call percolate down minheapi
+			exchange(array, index,minIndex);
+			minHeapify(array, minIndex);
+		}
+	}
+
+	private static void exchange(Integer[] array, int index1, int index2) {
+		int temp = array[index1];
+		array[index1] = array[index2];
+		array[index2] = temp;
 
 	}
 
 	public static void main(String[] args) {
-		Integer[] array = { 31, 1, 21, 5, 10, 12, 18, 3, 2, 8, 7 };
+		int[] array = { 5, 7, 8, 11, 56 };
 		Heap h = new Heap(array, array.length);
-		List<Integer> list = Arrays.asList(array);
-		System.out.println(list);
-		maxHeapify(array, 1);
-		System.out.println(Arrays.asList(array));
-        h.insert(45);
-        System.out.println(Arrays.asList(h.elements));
-        int i =0; 
-        int j = i-1/2;
-        System.out.println(j);
+//		List<Integer> list = Arrays.asList(array);
+		int[] a = {1,2,3,4,5};
+		h.buildHeap(array);
+		System.out.println(h);
+	/*	for (int i = 0; i < array.length; i++) {
+			maxHeapify(array, i);
+		}*/
+
+		System.out.println("heapify " + Arrays.asList(array));
+//		h.insert(45);
 	}
 
 	private void resizeHeap() {
-		capacity *= 2;
-		Integer[] newArray = new Integer[capacity * 2];
-		elements = Arrays.copyOf(elements, capacity * 2);
+		// capacity *= 2;
+		// Integer[] newArray = new Integer[capacity];
+		elements = Arrays.copyOf(elements, size * 2);
 
 	}
 
 	public void buildHeap(int[] input) {
-		if (input.length > capacity) {
+		if (input.length > size) {
 			resizeHeap();
 		}
 
 		for (int i = 0; i < input.length; i++) {
 			elements[i] = input[i];
 		}
-		
-		int n = size;
-		
-		for (int i = (size - 1) / 2; i >= 0; i--) {
+
+		int n = size-1;
+
+		for (int i = getParentIndex(n); i >= 0; i--) {
 			// percolate down
-			maxHeapify(elements, i);
+			maxHeapify( i);
 		}
 	}
-	
-	public void insert(int data){
-		if(size == capacity){
+
+	/**
+	 * This insert is for max heap
+	 * 
+	 * @param data
+	 */
+	public void insert(int data) {
+		if (size == capacity) {
 			resizeHeap();
 		}
-	// Increasing size to hold this new item
-		 size ++;
-		 
-		 int i = size-1;
-		 
-    // percolate up
- 		 while(i>=0 &&  data > elements[(i-1)/2] ){
-			 elements[i] = elements[(i-1)/2];
-			 if(i == 0) break;
-			 i = (i-1)/2;		 
-		 }
- 		 elements[i]=data;
+		
+		// Increasing size to hold this new item
+		size++;
+
+		int i = size - 1;
+
+		// percolate up
+		while (i >= 0 && data > elements[getParentIndex(i)]) {
+			//	elements[i] = elements[(i - 1) / 2];
+			// exchange
+			exchange(elements, i, getParentIndex(i));
+			if (i == 0)
+				break;
+			i = getParentIndex(i);
+		}
+
+		elements[i] = data;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return Arrays.toString(elements);
 	}
 }
