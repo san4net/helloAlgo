995389052ebf6ab108356762a50b803dae181694
san4net <san4net@gmail.com>
2016-03-20 12:42:01 +0530

    command pattern

---
+++ src/main/java/com/design/pattern/command/Light.java
@@ -0,0 +1,10 @@
+package com.design.pattern.command;
+// Receiver
+public class Light {
+	public void on(){
+		System.out.println("on");
+	}
+	public void off(){
+		System.out.println("off");
+	}
+}
