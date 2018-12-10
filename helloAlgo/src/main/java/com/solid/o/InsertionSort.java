aa64631ba6cc236448ca7fa9ce7042c3b913457d
santosh <san4net@gmail.com>
2018-12-10 12:05:35 +0530

    Add files via upload
    open close

---
+++ helloAlgo/src/main/java/com/solid/o/InsertionSort.java
@@ -0,0 +1,15 @@
+package com.solid.o;
+
+import java.util.Collection;
+
+public class InsertionSort implements Sort<Integer> {
+
+	@Override
+	public void sort(Collection<Integer> items) {
+		// TODO Auto-generated method stub
+		System.out.println("doing insertion sort");
+		
+	}
+	
+
+}
---
+++ helloAlgo/src/main/java/com/solid/o/MergeSort.java
@@ -0,0 +1,14 @@
+package com.solid.o;
+
+import java.util.Collection;
+
+public class MergeSort implements Sort<Integer> {
+
+	@Override
+	public void sort(Collection<Integer> items) {
+		// TODO Auto-generated method stub
+		System.out.println("doing merge sort");
+		
+	}
+
+}
