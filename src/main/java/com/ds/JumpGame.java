0d50c1f25b433c9e55ffe0d26bff10c8285e2666
santosh kumar <santosh-c.kumar@dbtechhackathon.com>
2021-08-29 21:00:39 +0530

    array algo

---
+++ src/main/java/com/ds/JumpGame.java
@@ -0,0 +1,20 @@
+package com.ds;
+
+/**
+ * 1. https://leetcode.com/problems/jump-game/
+ */
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int furthest_reach_so_far=0;
+        for(int i=0; i<=furthest_reach_so_far && i < nums.length; i++ ){
+            furthest_reach_so_far = Math.max(furthest_reach_so_far, i+nums[i]);
+
+            if(furthest_reach_so_far >=  nums.length-1){
+                return true;
+            }
+        }
+
+        return furthest_reach_so_far>=nums.length-1;
+
+    }
+}
